50eedbc1b231bf427d1f9516ab61ed125229d056
韦加金 <1192620781@qq.com>
2021-01-07 10:05:29 +0800

    feat: 推荐子菜单路由完成

---
+++ src/pages/discover/children-pages/recommend/index.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+export default memo(
+  function HYRecomment() {
+    return (
+      <div>
+        <h2>HYRecomment</h2>
+      </div>
+    )
+  }
+)
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,12 @@ import HYDiscover from "@/pages/discover";
 
 import HYMine from "@/pages/mine";
 import HYFriend from "@/pages/friend";
+import HYRcommend from "@/pages/discover/children-pages/recommend";
+import HYRanking from "@/pages/discover/children-pages/ranking";
+import HYSongs from "@/pages/discover/children-pages/songs";
+import HYArtist from "@/pages/discover/children-pages/artist";
+import HYDjradio from "@/pages/discover/children-pages/djradio";
+import HYAlbum from "@/pages/discover/children-pages/album";
 import { Redirect } from "react-router-dom";
 
 const routes = [
@@ -25,6 +31,31 @@ const routes = [
           <Redirect to="/discover/recommend" />
         )
       },
+      {
+        path: "/discover/recommend",
+        component: HYRcommend
+      },
+      {
+        path: "/discover/ranking",
+        component: HYRanking
+      },
+      {
+        path: "/discover/songs",
+        component: HYSongs
+      },
+      {
+        path: "/discover/artist",
+        component: HYArtist
+      },
+      {
+        path: "/discover/djradio",
+        component: HYDjradio
+      },
+      {
+        path: "/discover/album",
+        component: HYAlbum
+      },
+
     ]
   },
   {
